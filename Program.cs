5decd31b23b7f00c44c445168861ef5d8c650644
Vidhi Jadav <vjadav@mail.usf.edu>
2020-01-21 20:21:53 -0500

    Added computer information

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace Day_2_class
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("The addition of 1 and 10 is "+(1+10));
-
+            Console.WriteLine("Device name: " + Environment.MachineName);
+            Console.WriteLine("64 bit system: " + Environment.Is64BitOperatingSystem);
+            Console.WriteLine("User name: " + Environment.UserName);
         }
     }
 }
