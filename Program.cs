2d921b3e58f91dfbaf399464afa65e59a51ae70c
Vidhi Jadav <vjadav@mail.usf.edu>
2020-01-21 20:11:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day_2_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The addition of 1 and 10 is "+(1+10));
+
+        }
+    }
+}
